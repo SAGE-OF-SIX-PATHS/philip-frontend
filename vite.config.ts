8fb06a9428583b42fae85532384f90921e845cef
Akpamgbo Philip <nzubeakpamgbo>
2025-07-13 22:10:18 +0100

    add fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ✅ Simple Vite config, no history fallback here
 export default defineConfig({
   plugins: [react()],
 })
